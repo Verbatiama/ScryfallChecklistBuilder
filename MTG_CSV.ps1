3ffd7bb5ec5b4ade19d6bc9c1cd623745746c14c
Adam H <Verbatiama@gmail.com>
2023-06-18 15:40:06 +0930

    Initial commit - working script and ignore for output

---
+++ MTG_CSV.ps1
@@ -0,0 +1,24 @@
+function Remove-DuplicateByName {
+    begin {
+        $seenNames = @()
+    }
+
+    process {
+        if ($seenNames -notcontains $_.Name) {
+            $seenNames += $_.Name
+            $_
+        }
+    }
+
+    end {}
+}
+
+
+$regex = '^(?=.*\blegendary\b)(?=.*\bcreature\b).*$'
+Get-Content "default-cards-20230617210907.json" | 
+ConvertFrom-Json | 
+Where-Object { $_.type_line -match $regex  -and $_.reprint -ne "True" -and $_.oversized -ne "True" -and $_.variation -ne "True"  -and $_.games -match "paper" -and $_.collector_number -notmatch "★|T|b" -and $_.set_name -notmatch ".*\b(tokens|promos|Heroes of the Realm)\b.*"-and ($_.set_name -like "Un*" -or $_.legalities.vintage -eq "legal")} | 
+Sort-Object -Property @{Expression = "released_at"; Descending = $false},@{Expression = "set_name"; Descending = $false}, @{Expression = "collector_number"; Descending = $false} |
+Select-Object -Property name, set_name, {$_.prices.usd},{$_.prices.usd_foil}, released_at,  collector_number | 
+Remove-DuplicateByName |
+Export-CSV "result.csv"
