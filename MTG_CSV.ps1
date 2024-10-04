3fe75fd72c7934927eb84682e2a1a7d3d0b98d2c
Adam H <Verbatiama@gmail.com>
2024-10-04 14:22:24 +0930

    replaced file selector with a wild card

--- MTG_CSV.ps1
+++ MTG_CSV.ps1
@@ -15,7 +15,7 @@ function Remove-DuplicateByName {
 
 
 $regex = '^(?=.*\blegendary\b)(?=.*\bcreature\b).*$'
-Get-Content "default-cards-20230617210907.json" | 
+Get-Content "*.json" | 
 ConvertFrom-Json | 
 Where-Object { $_.type_line -match $regex  -and $_.reprint -ne "True" -and $_.oversized -ne "True" -and $_.variation -ne "True"  -and $_.games -match "paper" -and $_.collector_number -notmatch "★|T|b" -and $_.set_name -notmatch ".*\b(tokens|promos|Heroes of the Realm)\b.*"-and ($_.set_name -like "Un*" -or $_.legalities.vintage -eq "legal")} | 
 Sort-Object -Property @{Expression = "released_at"; Descending = $false},@{Expression = "set_name"; Descending = $false}, @{Expression = "collector_number"; Descending = $false} |
