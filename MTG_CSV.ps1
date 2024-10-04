a7c9f4f83c1c0daa61b9380e119ad90a402d2a13
Adam H <Verbatiama@gmail.com>
2024-10-04 14:22:51 +0930

    Formatted filter

--- MTG_CSV.ps1
+++ MTG_CSV.ps1
@@ -17,8 +17,19 @@ function Remove-DuplicateByName {
 $regex = '^(?=.*\blegendary\b)(?=.*\bcreature\b).*$'
 Get-Content "*.json" | 
 ConvertFrom-Json | 
-Where-Object { $_.type_line -match $regex  -and $_.reprint -ne "True" -and $_.oversized -ne "True" -and $_.variation -ne "True"  -and $_.games -match "paper" -and $_.collector_number -notmatch "★|T|b" -and $_.set_name -notmatch ".*\b(tokens|promos|Heroes of the Realm)\b.*"-and ($_.set_name -like "Un*" -or $_.legalities.vintage -eq "legal")} | 
-Sort-Object -Property @{Expression = "released_at"; Descending = $false},@{Expression = "set_name"; Descending = $false}, @{Expression = "collector_number"; Descending = $false} |
+Where-Object { $_.type_line -match $regex  -and `
+     $_.reprint -ne "True" -and `
+     $_.oversized -ne "True" -and `
+     $_.variation -ne "True"  -and `
+     $_.games -match "paper" -and `
+     $_.collector_number -notmatch "★|T|b" -and `
+     $_.set_name -notmatch ".*\b(tokens|promos|Heroes of the Realm)\b.*"-and `
+     ($_.set_name -like "Un*" -or $_.legalities.vintage -eq "legal")} | 
+ForEach-Object {
+            $number = ($_.collector_number -replace '[^\d]', '')
+            $_ | Add-Member -NotePropertyName 'collector_number_value' -NotePropertyValue ([int]($number)) -PassThru
+        } |
+Sort-Object -Property @{Expression = "released_at"; Descending = $false},@{Expression = "set_name"; Descending = $false}, @{Expression = {[int]$_.collector_number_value}; Descending = $false} |
 Select-Object -Property name, set_name, {$_.prices.usd},{$_.prices.usd_foil}, released_at,  collector_number | 
 Remove-DuplicateByName |
 Export-CSV "result.csv"
